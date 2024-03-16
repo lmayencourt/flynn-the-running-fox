09cbbbebfc3ed0b689ceb291f163a860aa1db2ab
Louis Mayencourt <louis.mayencourt@gmail.com>
2024-03-16 13:30:27 +0100

    Better world with multiples platforms

--- src/physics.rs
+++ src/physics.rs
@@ -57,30 +57,28 @@ pub fn collision (
     mut player_query: Query<(&Transform, &mut Player)>,
 ) {
     let (player_transform, mut player) = player_query.single_mut();
-    let ground = ground_query.single_mut();
+    for ground in ground_query.iter_mut() {
 
-    // let player_box = Aabb2d::new(player_transform.translation.truncate(), player_transform.scale.truncate()/2.0);
-    let ground_box = Aabb2d::new(ground.translation.truncate(), ground.scale.truncate()/2.0);
+        // let player_box = Aabb2d::new(player_transform.translation.truncate(), player_transform.scale.truncate()/2.0);
+        let ground_box = Aabb2d::new(ground.translation.truncate(), ground.scale.truncate()/2.0);
 
-    // A ray is an infinitely long line 
-    let ground_ray = Ray2d::new(player_transform.translation.truncate(), Vec2::NEG_Y);
-    // Ray cat have uses the origin and direction from ray, but have a finite length
-    let ray_cast = RayCast2d::from_ray(ground_ray, SPRITE_SIZE * player_transform.scale.y/2.0);
+        // A ray is an infinitely long line 
+        let ground_ray = Ray2d::new(player_transform.translation.truncate(), Vec2::NEG_Y);
+        // Ray cat have uses the origin and direction from ray, but have a finite length
+        let ray_cast = RayCast2d::from_ray(ground_ray, SPRITE_SIZE * player_transform.scale.y/2.0);
 
-    if let Some(point) = ray_cast.aabb_intersection_at(&ground_box) {
-        info!("Collision on ray at {}", point);
-        player.attitude = PlayerAttitude::Grounded;
+        if let Some(point) = ray_cast.aabb_intersection_at(&ground_box) {
+            info!("Collision on ray at {}", point);
+            player.attitude = PlayerAttitude::Grounded;
 
-        gizmos.circle_2d(ray_cast.ray.origin + *ray_cast.ray.direction * point, 10.0, Color::RED);
-    } else {
-        player.attitude = PlayerAttitude::InAir;
+            gizmos.circle_2d(ray_cast.ray.origin + *ray_cast.ray.direction * point, 10.0, Color::RED);
+            break;
+        } else {
+            player.attitude = PlayerAttitude::InAir;
+        }
+
+        // Debug physics by displaying gizmos line
+        gizmos.ray_2d(ground_ray.origin, *ground_ray.direction * ray_cast.max, Color::GRAY);
+        // gizmos.line_2d(ray_cast.ray.origin, ray_cast.ray.origin + *ray_cast.ray.direction * ray_cast.max, Color::GRAY);
     }
-
-    // Debug physics by displaying gizmos line
-    // let player_pos = player.translation.truncate();
-    // gizmos.ray_2d(player.translation.truncate(), Vec2::Y * SPRITE_SIZE * -player.scale.truncate()/2.0, Color::GREEN);
-    // gizmos.ray_2d(ground_ray.origin, ground_ray.direction.to * SPRITE_SIZE * -player.scale.truncate()/2.0, Color::GRAY);
-
-    gizmos.ray_2d(ground_ray.origin, *ground_ray.direction * ray_cast.max, Color::GRAY);
-    // gizmos.line_2d(ray_cast.ray.origin, ray_cast.ray.origin + *ray_cast.ray.direction * ray_cast.max, Color::GRAY);
 }
--- src/world.rs
+++ src/world.rs
@@ -7,7 +7,7 @@ use bevy::prelude::*;
 use crate::physics::{Collider, RigidBody};
 
 const GROUND_HEIGHT: f32 = 20.0;
-const GROUND_SIZE: Vec3 = Vec3::new(800.0, GROUND_HEIGHT, 0.0);
+const GROUND_SIZE: Vec3 = Vec3::new(250.0, GROUND_HEIGHT, 0.0);
 
 const OBSTACLE_SIZE: Vec3 = Vec3::new(20.0, 20.0, 0.0);
 
@@ -36,6 +36,11 @@ pub fn setup(
     Ground,
     Collider));
 
+    // Ground
+    spawn_platform(&mut commands, -350.0, GROUND_HEIGHT, GROUND_SIZE.x, GROUND_SIZE.y);
+    spawn_platform(&mut commands, 0.0, -GROUND_HEIGHT, GROUND_SIZE.x, GROUND_SIZE.y);
+    spawn_platform(&mut commands, 350.0, -GROUND_HEIGHT, GROUND_SIZE.x, GROUND_SIZE.y);
+
     // Obstacle
     commands.spawn((SpriteBundle {
         transform: Transform {
@@ -51,4 +56,21 @@ pub fn setup(
     },
     Obstacle,
     ));
+}
+
+fn spawn_platform(commands: &mut Commands, x: f32, y: f32, h: f32, l: f32) {
+    commands.spawn((SpriteBundle {
+        transform: Transform {
+            translation: Vec3::new(x, y, 0.0),
+            scale: Vec3::new(h, l, 0.0),
+            ..default()
+        },
+        sprite: Sprite {
+            color: Color::rgb(1.0, 1.0, 1.0),
+            ..default()
+        },
+        ..default()
+    },
+    Ground,
+    Collider));
 }
